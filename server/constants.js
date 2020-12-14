29993ab3caeb5c34620388e98d8d2d9d908b0233
Caleb Matthews <calebjmatthews@gmail.com>
2020-12-14 17:34:05 -0600

    Addition of buildings_construction table

--- server/constants.js
+++ server/constants.js
@@ -1,4 +1,4 @@
-const TABLE_NAMES = ['vault', 'research_status', 'buildings',
+const TABLE_NAMES = ['vault', 'research_status', 'buildings', 'buildings_construction',
   'research_option_decks', 'timers', 'trading_status', 'accounts'];
 
 module.exports = { TABLE_NAMES };
