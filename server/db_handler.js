c21a1a65337532c7c56604fd3ff9979735c8bf71
Caleb Matthews <calebjmatthews@gmail.com>
2020-12-19 15:46:41 -0600

    Addition of production db settings

--- server/db_handler.js
+++ server/db_handler.js
@@ -3,10 +3,14 @@ const mysql = require('promise-mysql');
 class DataBaseHandler {
   constructor() {
     mysql.createPool({
-      host: process.env.DEV_HOST,
-      user: process.env.DEV_MYSQL_USER,
-      password: process.env.DEV_MYSQL_PASSWORD,
-      database: process.env.DEV_MYSQL_DB,
+      host: (process.env.NODE_ENV == 'production')
+          ? process.env.PROD_HOST : process.env.DEV_HOST,
+      user: (process.env.NODE_ENV == 'production')
+          ? process.env.PROD_MYSQL_USER : process.env.DEV_MYSQL_USER,
+      password: (process.env.NODE_ENV == 'production')
+          ? process.env.PROD_MYSQL_PASSWORD : process.env.DEV_MYSQL_PASSWORD,
+      database: (process.env.NODE_ENV == 'production')
+          ? process.env.PROD_MYSQL_DB : process.env.DEV_MYSQL_DB,
       waitForConnections: true,
       connectionLimit: 50,
       queueLimit: 0
