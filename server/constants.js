287dabf58c71431ef90c2476249c37a4e99020b1
Caleb Matthews <calebjmatthews@gmail.com>
2020-12-30 15:47:15 -0600

    Addition of leader and equipment table names

--- server/constants.js
+++ server/constants.js
@@ -1,4 +1,5 @@
 const TABLE_NAMES = ['vault', 'research_status', 'buildings', 'buildings_construction',
-  'research_option_decks', 'timers', 'trading_status', 'accounts'];
+  'research_option_decks', 'timers', 'trading_status', 'accounts', 'leaders',
+  'equipment'];
 
 module.exports = { TABLE_NAMES };
