18005416ec245709ebac974b2d48c8c8d76ce878
Caleb Matthews <calebjmatthews@gmail.com>
2021-09-27 09:44:16 -0500

    Conversation state table

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,5 @@
 const TABLE_NAMES = ['vault', 'research_status', 'buildings', 'buildings_construction',
   'research_option_decks', 'timers', 'trading_status', 'accounts', 'leaders',
-  'equipment'];
+  'equipment', 'conversation_status'];
 
 module.exports = { TABLE_NAMES };
