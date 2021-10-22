d1469cc7f077fafeac32816e21e7834c50088719
Caleb Matthews <calebjmatthews@gmail.com>
2021-10-22 13:03:11 -0500

    Buildings storage table

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,5 @@
 const TABLE_NAMES = ['vault', 'research_status', 'buildings', 'buildings_construction',
-  'research_option_decks', 'timers', 'trading_status', 'accounts', 'leaders',
-  'equipment', 'conversation_status'];
+  'buildings_storage', 'research_option_decks', 'timers', 'trading_status', 'accounts',
+  'leaders', 'equipment', 'conversation_status'];
 
 module.exports = { TABLE_NAMES };
