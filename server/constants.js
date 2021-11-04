83f1b45302c8c1a4b7a1de5d80f4b3d12f9e4779
Caleb Matthews <calebjmatthews@gmail.com>
2021-11-04 16:25:17 -0500

    Quest status table

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,5 @@
 const TABLE_NAMES = ['vault', 'research_status', 'buildings', 'buildings_construction',
   'buildings_storage', 'research_option_decks', 'timers', 'trading_status', 'accounts',
-  'leaders', 'equipment', 'conversation_status'];
+  'leaders', 'equipment', 'conversation_status', 'quest_status'];
 
 module.exports = { TABLE_NAMES };
