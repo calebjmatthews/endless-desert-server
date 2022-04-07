a9d49774afefc6e8c1f60049e8287f2c710ca5bd
Caleb Matthews <calebjmatthews@gmail.com>
2022-04-06 22:56:47 -0500

    Messages table

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,5 @@
 const TABLE_NAMES = ['vault', 'research_status', 'buildings', 'buildings_construction',
   'buildings_storage', 'research_option_decks', 'timers', 'trading_status', 'accounts',
-  'leaders', 'equipment', 'conversation_status', 'quest_status'];
+  'leaders', 'equipment', 'conversation_status', 'quest_status', 'messages'];
 
 module.exports = { TABLE_NAMES };
