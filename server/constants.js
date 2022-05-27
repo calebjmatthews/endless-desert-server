6bc69253afb6dedad22ac753274b4c8e988fe0ea
Caleb Matthews <calebjmatthews@gmail.com>
2022-05-27 16:43:23 -0500

    Add terrain table

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,5 @@
 const TABLE_NAMES = ['vault', 'research_status', 'buildings', 'buildings_construction',
   'buildings_storage', 'research_option_decks', 'timers', 'trading_status', 'accounts',
-  'leaders', 'equipment', 'conversation_status', 'quest_status', 'messages'];
+  'leaders', 'equipment', 'conversation_status', 'quest_status', 'messages', 'terrain'];
 
 module.exports = { TABLE_NAMES };
